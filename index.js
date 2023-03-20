991b7d69c6ea41ced19ee27c08c9c3001f71882e
Omar Qaqish <omqaqish@gmail.com>
2023-03-20 20:37:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+const names = ["Charlie", "Samip", "Ali"];
+const event = "birthday";
+
+function writeCards(names, event) {
+  const messages = [];
+  for (let i = 0; i < names.length; i++) {
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    messages.push(message);
+ }
+  return messages;
+}
+const messages = writeCards(names, event);
+
+function countDown(i) {
+    while (i >= 0) {
+      console.log(i);
+      i--;
+    }
+  }
